84a55b80ca1b318232ad6aebf47fe864f1e72608
neny <kaunaini338@gmail.com>
2023-09-16 23:30:19 +0700

    build vite configuration

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: "index.html",
-        blog: "blog/index.html",
+        "cek-harga": "cek-harga/index.html",
+        contact: "contact/index.html",
+        lacak: "lacak/index.html",
+        tentang: "tentang/index.html",
+        login: "login/index.html",
+        register: "register/index.html",
+        layanan: "layanan/index.html",
+        kirim: "kirim/index.html",
       },
     },
   },
