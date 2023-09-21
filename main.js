a1c59ba4daef15096500ccecabd68052cd024064
Denny Setyawan Huda <denycam9@gmail.com>
2023-09-21 16:47:08 +0700

    feat: improve code style

--- main.js
+++ main.js
@@ -59,3 +59,23 @@ if (window.location.pathname.split("/")[1] === "cek-harga") {
     });
   });
 }
+
+if (window.location.pathname.split("/")[1] === "register") {
+  const registerFormElement = document.querySelector(".register__form");
+  registerFormElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData(registerFormElement);
+
+    fetch("https://dummyjson.com/users/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: data.get("name"),
+        password: data.get("password"),
+        email: data.get("email"),
+      }),
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  });
+}
