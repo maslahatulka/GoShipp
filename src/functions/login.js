97fd7dc072bf69267e749421dfb6cd6069e4a726
neny <kaunaini338@gmail.com>
2023-10-10 10:05:59 +0700

    add toastify

--- src/functions/login.js
+++ src/functions/login.js
@@ -1,3 +1,6 @@
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
 const login = () => {
   if (window.location.pathname.split("/")[1] === "login") {
     const loginFormElement = document.querySelector(".login__form");
@@ -20,8 +23,30 @@ const login = () => {
           const responseData = await response.json();
           console.log(responseData);
           localStorage.setItem("token", responseData.token);
-          window.location.href = "/kirim/";
+
+          Toastify({
+            text: "Login Berhasil",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            style: {
+              background: "#6EF063",
+            },
+          }).showToast();
+
+          setTimeout(() => {
+            window.location.href = "/kirim/";
+          }, 3000);
         } else {
+          Toastify({
+            text: "Login Gagal",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            style: {
+              background: "rgb(255,0,0)",
+            },
+          }).showToast();
           console.error("Failed to log in. Status code:", response.status);
         }
       } catch (error) {
--- src/functions/register.js
+++ src/functions/register.js
@@ -1,21 +1,57 @@
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
 const register = () => {
   if (window.location.pathname.split("/")[1] === "register") {
     const registerFormElement = document.querySelector(".register__form");
-    registerFormElement.addEventListener("submit", (event) => {
+
+    registerFormElement.addEventListener("submit", async (event) => {
       event.preventDefault();
       const data = new FormData(registerFormElement);
+      try {
+        const response = await fetch(
+          "https://be-palembang-21-production.up.railway.app/new",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: data.get("username"),
+              password: data.get("password"),
+              email: data.get("email"),
+            }),
+          }
+        );
 
-      fetch("https://be-palembang-21-production.up.railway.app/new", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          username: data.get("username"),
-          password: data.get("password"),
-          email: data.get("email"),
-        }),
-      })
-        .then((res) => res.json())
-        .then(console.log);
+        if (response.ok) {
+          const responseData = await response.json();
+          Toastify({
+            text: "Berhasil membuat akun",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            style: {
+              background: "#6EF063",
+            },
+          }).showToast();
+
+          setTimeout(() => {
+            window.location.href = "/login/";
+          }, 3000);
+        } else {
+          Toastify({
+            text: "Gagal membuat akun",
+            duration: 3000,
+            gravity: "top",
+            position: "center",
+            style: {
+              background: "rgb(255,0,0)",
+            },
+          }).showToast();
+          console.error("Failed to log in. Status code:", response.status);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     });
   }
 };
